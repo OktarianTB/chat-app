5d8e94fe42d80e63088e4eb6e7bb096501c778f5
Oktarian <oktotb@gmail.com>
2020-08-22 12:06:17 +0200

    make it better2

--- create_db.py
+++ create_db.py
@@ -1,19 +1,7 @@
-from flask import Flask
+from application import app
 from models import *
-import os
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI']=os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
-
-db.init_app(app)
 
 
-def main():
+with app.app_context():
     db.create_all()
 
-
-if __name__ == "__main__":
-    with app.app_context():
-        main()
